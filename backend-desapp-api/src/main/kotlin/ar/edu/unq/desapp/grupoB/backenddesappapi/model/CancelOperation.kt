b7f29b888ffb96176215e6ef5e210f52345dc5e9
MelinaJimenez <31451170+MelinaJimenez@users.noreply.github.com>
2023-11-29 17:44:18 -0300

    Update CancelOperation.kt

--- backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupoB/backenddesappapi/model/CancelOperation.kt
+++ backend-desapp-api/src/main/kotlin/ar/edu/unq/desapp/grupoB/backenddesappapi/model/CancelOperation.kt
@@ -3,12 +3,15 @@ package ar.edu.unq.desapp.grupoB.backenddesappapi.model
 class CancelOperation : OperationType() {
 
     override val type = "Cancel"
-    override fun realizeAction(operation: Operation) : Operation {
+    override fun realizeAction(operation: Operation) : Operation? {
 
+        var operationCurrent = operation
         var user = operation.userCreated
+        if(operation.userCreated != null){
+            operationCurrent = user!!.sustractPoint(operation)!!
+        }
 
-
-        return  user!!.sustractPoint(operation)
+        return  operationCurrent
     }
 
 }
